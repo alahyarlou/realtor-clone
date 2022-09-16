e119d76519e29aaa5c1021a3a91b4d560f5a2c2d
ali alahyarlou <alialahyarlou@gmail.com>
2022-09-16 22:32:22 +0430

    create pages and config routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./Pages/SignIn";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Offers from "./Pages/Offers";
+import Profile from "./Pages/Profile";
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
